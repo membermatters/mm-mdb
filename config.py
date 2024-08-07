9349288a3f45dcdd5b658b434aee77211b2cb1ea
Jaimyn Mayer <hello@jaimyn.dev>
2024-08-07 15:24:27 +1000

    added default config options

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-import logging
+from default_config import *
 
+# use this file to override defaults in the default_config.py file
 MDB_LOG_LEVEL = logging.INFO
-MDB_LOG_ACK = False
-MM_LOG_LEVEL = logging.INFO
-PING_PERIOD = 5
-PROCESS_AFFINITY = 3
-MIN_CARD_SCAN_VALUE = 100  # ignore all card scans with an ID lower than this amount (useful if you occasionally get noise on your Wiegand line resulting in erronous scans with low values)
-WIEGAND_32BIT_MODE = True
+MM_LOG_LEVEL = logging.DEBUG
+
 API_SECRET = "xyz.xyzabc"
 PORTAL_WS_URL = "wss://portal.blah.blah/ws/access/memberbucks/"
-
---
+++ default_config.py
@@ -0,0 +1,12 @@
+import logging
+
+MDB_LOG_LEVEL = logging.INFO
+MDB_LOG_ACK = False
+MM_LOG_LEVEL = logging.INFO
+PING_PERIOD = 5
+PROCESS_AFFINITY = 3
+MIN_CARD_SCAN_VALUE = 100  # ignore all card scans with an ID lower than this amount (useful if you occasionally get noise on your Wiegand line resulting in erronous scans with low values)
+WIEGAND_32BIT_MODE = True
+
+API_SECRET = "xyz.xyzabc"
+PORTAL_WS_URL = "wss://portal.blah.blah/ws/access/memberbucks/"
